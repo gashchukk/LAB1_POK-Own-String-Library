9ecc6a9e31639fc68373c49fb40edbb03287776a
lizaArnauta <arnauta.pn@ucu.edu.ua>
2023-09-27 17:39:15 +0200

    substr, erase methods

--- main.cpp
+++ main.cpp
@@ -6,6 +6,21 @@
 
 int main(int argc, char* argv[]) {
     my_str_t s(1, 'A');
+
+    //ліза
+    my_str_t s1(5,'Hello');
+    my_str_t s2(5,'world');
+    s1.append(s2);
+    s1.append('!');
+    s1.append(".Sunny day");
+
+    s1.erase(1,2);
+
+    size_t search1 = s1.find('lo');
+    size_t search2= s1.find("day");
+    size_t search3 = s1.find("orld");
+
+    my_str_t substr = s1.substr(3, 2);
     std::cout << "4568";
     return 0;
 }
--- my_str_t.cpp
+++ my_str_t.cpp
@@ -307,6 +307,7 @@ void my_str_t::append(char c) {
 
 void my_str_t::append(const char* cstr){
     size_t length_cstr = strlen(cstr);
+
     if (size_m + length_cstr >= capacity_m){
         size_t capacity_append = (size_m+length_cstr) + (16 - ((size_m+length_cstr)%16));
         reserve(capacity_append);
@@ -333,6 +334,21 @@ void my_str_t::append(const my_str_t& str){
     size_m+=length_str;
     data_m[size_m] = '\0';
 
+}
+
+//erase
+
+void my_str_t::erase(size_t begin, size_t size) {
+    if(size + begin > size_m){
+        size = size_m - begin;
+    }
+
+    for(size_t i = begin; i < size_m - size; ++i){
+        data_m[i] = data_m[size + i];
+    }
+
+    size_m = size_m - size;
+    data_m[size_m] = '\0';
 }
 
 <<<<<<< HEAD
@@ -388,6 +404,26 @@ size_t my_str_t::find(const char* cstr, size_t idx){ //третій find, за �
         }
     }
     return -1;
+}
+
+my_str_t my_str_t::substr(size_t begin, size_t size) {
+    if (size + begin > size_m){
+        size = size_m - begin;
+    }
+
+    //створимо нову сабстрінгу
+    char* new_str = new char[size + 1];
+
+    //перекопіюємо в неї симовли
+    for (size_t i = 0; i < size; ++i){
+        new_data[i] = data_m[begin + i];
+    }
+
+    new_str[size] = '\0';
+    my_str_t result(new_str);
+    delete[] new_str; // обов'язково звільняємо пам'ять
+
+    return result;
 }
 
 my_str_t::~my_str_t() {
