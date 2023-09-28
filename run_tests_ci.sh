17116e26a61056b58595049438e9111f16220a6a
Teodor Muzychuk <teodor.muzychuk@ucu.edu.ua>
2023-09-28 17:09:33 +0300

    fixed ci to use self-hosted runner

--- run_tests_ci.sh
+++ run_tests_ci.sh
@@ -5,6 +5,10 @@ lab_dir=$(pwd)
 echo "Lab directory: $lab_dir"
 cd ..
 pwd
+if [ -e UCU_autotests ]; then
+		echo "Removing old autotests"
+		rm -rf UCU_autotests
+fi
 git clone https://github.com/DariaMinieieva/UCU_autotests
 cd UCU_autotests/c_strings_auto_testing
 cp -r $lab_dir ./c_str_lib
@@ -13,3 +17,6 @@ echo "Starting compile"
 ./compile.sh
 echo "Starting tests"
 ./bin/gtester
+echo "Cleaning up"
+cd ../..
+rm -rf UCU_autotests
